a9c82050e5cbdde0b298dc5697a0afff2732e58c
johan <johanwirasugianto@gmail.com>
2022-09-30 16:22:17 +0700

    Initial project Springboot

---
+++ src/main/java/co/id/mii/serversidesibkm/ServersideSibkmApplication.java
@@ -0,0 +1,13 @@
+package co.id.mii.serversidesibkm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideSibkmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersideSibkmApplication.class, args);
+	}
+
+}
